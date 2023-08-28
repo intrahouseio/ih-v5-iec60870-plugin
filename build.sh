1c3af617d70a9b304d9308e363d0e84b4b607e0f
goodspeed1986 <goodspeed1986@gmail.com>
2023-08-28 12:00:37 +0300

    add bin folder

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 cd lib60870-C/examples/cs104_client
-sudo gcc -g -o client104_darwin_arm64 simple_client_2.c -I../../src/inc/api -I../../src/hal/inc -I../../src/tls ../../build_mac/lib60870_darwin_arm64.a -lpthread
+sudo gcc -g -o ../../../bin/client104_darwin_arm64 simple_client_2.c -I../../src/inc/api -I../../src/hal/inc -I../../src/tls ../../build_mac/lib60870_darwin_arm64.a -lpthread
--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,8 @@
 cd lib60870-C/examples/cs104_client
-make PROJECT_BINARY_NAME=client104_linux_amd64 TARGET=LINUX-X64
-make PROJECT_BINARY_NAME=client104_linux_arm TARGET=LINUX-ARM
-make PROJECT_BINARY_NAME=client104_linux_arm64 TARGET=LINUX-ARM64
-make PROJECT_BINARY_NAME=client104_windows_amd64.exe TARGET=WIN64
+make PROJECT_BINARY_NAME=../../../bin/client104_linux_amd64 TARGET=LINUX-X64
+make PROJECT_BINARY_NAME=../../../bin/client104_linux_arm TARGET=LINUX-ARM
+make PROJECT_BINARY_NAME=../../../bin/client104_linux_arm64 TARGET=LINUX-ARM64
+make PROJECT_BINARY_NAME=../../../bin/client104_windows_amd64.exe TARGET=WIN64
 
 #sudo gcc -g -o client104_linux_amd64 simple_client_2.c -I../../src/inc/api -I../../src/hal/inc -I../../src/tls ../../build/liblib60870.a -lpthread
 #sudo arm-linux-gnueabi-gcc -mno-unaligned-access -g -o client104_linux_arm simple_client_2.c -I../../src/inc/api -I../../src/hal/inc -I../../src/tls ../../build-arm/liblib60870.a -lpthread
